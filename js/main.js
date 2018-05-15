11eaf8d23625c2c2495ddb63690b23a68bad534c
Mohcin Bounouara <rapmabiz@gmail.com>
2018-05-15 16:31:41 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function main() {
+    (function() {
+        'use strict';
+
+        $("span").click(function () {
+            $(".showed-menu").toggle("slow");  // showed ul menu with toggle effect
+        });
+
+
+    }());
+}
+main();
